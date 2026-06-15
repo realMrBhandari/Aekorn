156d0c24458d6368554f6509a62bd7ccb0bd9c56
realMrBhandari <23021086@geu.ac.in>
2026-06-15 19:49:56 +0530

    refactor: updated sql dependencies

--- core/onboarding.py
+++ core/onboarding.py
@@ -4,85 +4,66 @@ from utilities.cli_input_helpers import amount_processor
 
 
 def start_Onboarding():
-    # ? Making connection with database
-    connect = sqlite3.connect("database/fincil.db")
+    connect = sqlite3.connect("database/FinCLI.db")
     cursor = connect.cursor()
 
-    # ? accounts table schema execution
-    cursor.execute("""
-        CREATE TABLE IF NOT EXISTS accounts (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        bank_account_name TEXT NOT NULL,
-        running_balance REAL NOT NULL,
-        recorded_on TEXT DEFAULT(datetime('now')) NOT NULL)
-    """)
+    #!  executing all the schemas
+    schemas = [
+        "sql/schema/accounts.sql",
+        "sql/schema/income_expense.sql",
+        "sql/schema/obligations.sql",
+        "sql/schema/people.sql",
+        # "sql/schema/transactions.sql",
+    ]
+    for file_name in schemas:
+        with open(file_name, "r") as file:
+            cursor.executescript(file.read())
+
     connect.commit()
 
-    # ? Onboarding screen message
+    #!   function to take bank account data and log it
+    def get_bankAccount_input():
+        account_name = input("Name of your bank: ")
+        running_balance = amount_processor()
+        with open("sql/insert/accounts.sql", "r") as file:
+            cursor.execute(file.read(), (account_name, running_balance))
+
+        connect.commit()
+
+    #!  Onboarding screen message
     print(
         "\033[1;32mWelcome to FinCLI. Let's add your bank accounts and personalize your financial experience.\033[0m"
     )
 
-    # ?  function to take bank account data and log it
-    def get_bankAccount_input():
-        account_name = input("Name of your bank: ")
-        running_balance = amount_processor()
-        cursor.execute(f"""INSERT INTO accounts
-                   (bank_account_name, running_balance)
-                   VALUES
-                   ("{account_name}", "{running_balance}");""")
-        connect.commit()
-
     get_bankAccount_input()
 
-    # ? Decision for adding another bank account
-    add_another_account = input(
-        "\n\033[33mWould you like to add another bank account? (y/n): \033[0m"
-    )
-    valid_responses = [
-        "y",
-        "Y",
-        "YES",
-        "Yes",
-        "yes",
-        "n",
-        "N",
-        "NO",
-        "No",
-        "no",
-    ]
+    #! Decision for adding another bank account
 
-    while add_another_account not in valid_responses:
-        print("\033[31m Incorrect input! Try again\033[0m")
-        add_another_account = input(
-            "\n\033[33mWould you like to add another bank account? (y/n): \033[0m"
+    valid_responses = ["y", "yes", "n", "no"]
+
+    while True:
+        add_another_account = (
+            input(
+                "\n\033[33mWould you like to add another bank account? (y/n): \033[0m"
+            )
+            .strip()
+            .lower()
         )
 
-    while add_another_account in ["y", "Y", "YES", "Yes", "yes"]:
-        get_bankAccount_input()
-        add_another_account = input(
-            "\n\033[33mWould you like to add another bank account? (y/n): \033[0m"
-        )
-        valid_responses = [
-            "y",
-            "Y",
-            "YES",
-            "Yes",
-            "yes",
-            "n",
-            "N",
-            "NO",
-            "No",
-            "no",
-        ]
         while add_another_account not in valid_responses:
-            print("\033[31m Incorrect input! Try again\033[0m")
+            print("\033[31mIncorrect input! Try again.\033[0m")
+            add_another_account = (
+                input(
+                    "\n\033[33mWould you like to add another bank account? (y/n): \033[0m"
+                )
+                .strip()
+                .lower()
+            )
 
-    # ? Fetching logged data from db
-    def fetch_all_bankAccount_info():
-        cursor.execute("SELECT * FROM accounts")
-        accounts_info = cursor.fetchall()
-        return accounts_info
+        if add_another_account in ["n", "no"]:
+            break
+
+        get_bankAccount_input()
 
     cursor.close()
     connect.close()
--- sql/schema/accounts.sql
+++ sql/schema/accounts.sql
@@ -2,4 +2,5 @@ CREATE TABLE IF NOT EXISTS accounts (
         account_id INTEGER PRIMARY KEY AUTOINCREMENT,
         bank_account_name TEXT NOT NULL,
         running_balance REAL NOT NULL,
-        created_on TEXT DEFAULT(datetime('now')) NOT NULL)
+        created_on TEXT DEFAULT(datetime('now')) NOT NULL
+        );
